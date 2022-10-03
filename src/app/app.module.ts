32f6069cf3533dc5c65183f74c9eff4d223c898d
nahir1009 <nahir1009@gmail.com>
2022-10-03 18:14:35 -0300

    primer angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { MainComponent } from './componentes/main/main.component';
+import { FooterComponent } from './componentes/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    MainComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
